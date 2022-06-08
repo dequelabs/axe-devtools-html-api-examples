4dcc826510e476299ec8ac39f83e83c97801b5a4
ankit-dq <ankit.kathal@deque.com>
2022-06-08 19:08:58 +0530

    Adding the vue-browser example project files

---
+++ Node/vue-browser/tests/unit/example.spec.ts
@@ -0,0 +1,36 @@
+import { mount } from '@vue/test-utils'
+import HelloWorld from '@/components/HelloWorld.vue'
+import axeDevtools from '@axe-devtools/browser';
+import Reporter from '@axe-devtools/reporter'
+import rimraf from 'rimraf'
+
+const fixture = document.createElement('div');
+document.body.appendChild(fixture);
+// let reporter;
+describe('HelloWorld.vue', () => {
+  let reporter;
+  
+  beforeEach(() => {
+    rimraf.sync('./a11y-results*')
+    reporter = new Reporter('A11yResults', './a11y-results')
+    })
+
+  afterEach(() => {
+    reporter.buildHTML('./a11y-results')
+    reporter.buildCSV('./a11y-results')
+    reporter.buildJUnitXML('./a11y-results')
+    })
+
+  it('renders props.msg when passed',async () => {
+    const msg = 'new message'
+    const wrapper = mount(HelloWorld, {
+      attachTo: fixture,
+      props: { msg }
+    })
+    const results = await axeDevtools.run(fixture);
+    console.log(results);
+    reporter.logTestResult('vue-browser-component', results)
+    expect(wrapper.text()).toMatch(msg)
+    
+  })
+})
