8a460031a534df52adfef0d468213edb63933198
pschwendy <peterschwende@gmail.com>
2022-06-17 14:55:56 -0400

    Update axe-devtools-selenium version

--- C#/MSTest/Example.cs
+++ C#/MSTest/Example.cs
@@ -1,11 +1,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium.Remote;
-using ReportResults = Deque.AxeDevtools.Results.ReportResults;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
 using Deque.AxeDevtools.Selenium;
-using System;
-using RunTagType = Deque.AxeDevtools.RunOptions.TagType;
 
 
 // C# Axe example using Selenium and MSTest to run tests
@@ -13,14 +10,12 @@ using RunTagType = Deque.AxeDevtools.RunOptions.TagType;
 namespace AxeTest {
     [TestClass]
     public class Example {
-        static RemoteWebDriver driver;
-        static AxeSelenium axe;
+        static IWebDriver driver;
 
         [ClassInitialize]
         public static void AssemblyInit(TestContext context) {
             driver = new ChromeDriver();
             driver.Navigate().GoToUrl("https://broken-workshop.dequelabs.com/");
-            axe = new AxeSelenium(driver);
         }
 
         [ClassCleanup]
@@ -30,12 +25,14 @@ namespace AxeTest {
 
         [TestMethod]
         public void HompageNoFlow() {
+            AxeSelenium axe = new AxeSelenium(driver);
             var results = axe.Analyze();
             Assert.AreEqual(0, results.Findings.Violations.Count);
         }
 
         [TestMethod]
         public void AlteredHompage() {
+            AxeSelenium axe = new AxeSelenium(driver);
             driver.FindElement(By.CssSelector("#main-content > div.Recipes > div:nth-child(1) > div.Recipes__card-foot > button")).Click();
             var results2 = axe.Analyze();
             Assert.AreEqual(0, results2.Findings.Violations.Count);
