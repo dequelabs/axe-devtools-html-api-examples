697cb95eeda32631108af1588445f2a492f07028
pschwendy <peterschwende@gmail.com>
2022-06-17 14:23:59 -0400

    Made axe-devtools-selenium up to date

--- C#/Selenium/Example.cs
+++ C#/Selenium/Example.cs
@@ -3,21 +3,27 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Remote;
 using System;
-using ReportResults = Deque.AxeDevtools.Results.ReportResults;
 
 
 // C# Axe example using Selenium
 
-RemoteWebDriver driver = new ChromeDriver();
-driver.Navigate().GoToUrl("https://broken-workshop.dequelabs.com/");
+public class Example {
+    public static void Main() {
+        IWebDriver driver = new ChromeDriver();
+        driver.Navigate().GoToUrl("https://broken-workshop.dequelabs.com/");
 
-AxeSelenium axe = new AxeSelenium(driver);
+        AxeSelenium axe = new AxeSelenium(driver);
 
-var results = axe.Analyze();
-Console.WriteLine("Found " + results.Findings.Violations.Count + " violations!");
+        var results = axe.Analyze();
+        Console.WriteLine("Found " + results.Findings.Violations.Count + " violations!");
+        
+        // Create a new axe driver to avoid re-injection of scripts
+        axe = new AxeSelenium(driver);
+        
+        driver.FindElement(By.CssSelector("#main-content > div.Recipes > div:nth-child(1) > div.Recipes__card-foot > button")).Click();
+        var results2 = axe.Analyze();
+        Console.WriteLine("Found " + results2.Findings.Violations.Count + " violations!");
 
-driver.FindElement(By.CssSelector("#main-content > div.Recipes > div:nth-child(1) > div.Recipes__card-foot > button")).Click();
-var results2 = axe.Analyze();
-Console.WriteLine("Found " + results2.Findings.Violations.Count + " violations!");
-
-driver.Quit();
+        driver.Quit();
+    }
+}
