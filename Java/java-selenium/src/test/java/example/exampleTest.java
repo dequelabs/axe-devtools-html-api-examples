3f6102061271a4b9973e68b8f345867e6305b274
ankit-dq <ankit.kathal@deque.com>
2022-07-01 15:33:11 +0530

    Update exampleTest.java

--- Java/java-selenium/src/test/java/example/exampleTest.java
+++ Java/java-selenium/src/test/java/example/exampleTest.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.IOException;
 
 import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
