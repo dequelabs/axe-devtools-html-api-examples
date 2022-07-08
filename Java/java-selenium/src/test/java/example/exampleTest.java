bbb6313cecbca6757e5f2d06e8e0d116fc6712bb
ankit-dq <ankit.kathal@deque.com>
2022-07-08 20:33:49 +0530

    Incorporated the requested changes

--- Java/java-selenium/src/test/java/example/exampleTest.java
+++ Java/java-selenium/src/test/java/example/exampleTest.java
@@ -2,6 +2,7 @@ package example;
 
 import java.io.File;
 import java.io.IOException;
+
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -13,58 +14,60 @@ import com.deque.html.axecore.results.Results;
 import com.deque.html.axedevtools.selenium.*;
 import com.deque.html.axedevtools.selenium.reporter.*;
 
-
-
 public class exampleTest {
-    static String reporter = new File("src/test/resources/reporter-cli-macos").getAbsolutePath();
-    public static AxeReportingOptions _reportOptions = new AxeReportingOptions();
-    public static WebDriver driver = null;
-    public static AxeDriver axedriver = null;
-    public static AxeSelenium axeSelenium = null;
-    public static String Logger;
-    public static String Destination;
+  private static AxeReportingOptions _reportOptions = new AxeReportingOptions();
+  private static WebDriver driver = null;
+  private static AxeDriver axedriver = null;
+  private static AxeSelenium axeSelenium = null;
 
-    @BeforeClass
-    public static void initiate_drivers(){
-        _reportOptions = new AxeReportingOptions();
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        axedriver = new AxeDriver(driver);
-        axeSelenium = new AxeSelenium();
-        AxeConfiguration.configure()
-                .testSuiteName("Test")
-                .outputDirectory("axe-json-reports/");
-        Logger =  new File("axe-json-reports/").getAbsolutePath();
-        Destination =  new File("a11y-results/").getAbsolutePath();
+  @BeforeClass
+  public static void initiate_drivers() {
+    _reportOptions = new AxeReportingOptions();
+    WebDriverManager.chromedriver().setup();
+    driver = new ChromeDriver();
+    axedriver = new AxeDriver(driver);
+    axeSelenium = new AxeSelenium();
+    AxeConfiguration.configure().testSuiteName("Test").outputDirectory("axe-json-reports/");
+  }
 
+  @Test
+  public void test_a11y_workshop_homepage() {
+    driver.get("https://broken-workshop.dequelabs.com/");
+    Results results1 =
+        axeSelenium.logResults(_reportOptions.uiState("Homepage_scan")).run(axedriver);
+  }
+
+  @Test
+  public void test_recipe_card_a11y_workshop_homepage() {
+    driver.get("https://broken-workshop.dequelabs.com/");
+    driver
+        .findElement(
+            By.cssSelector(
+                "#main-content > div.Recipes > div:nth-child(1) > div.Recipes__card-foot > button"))
+        .click();
+    Results results2 =
+        axeSelenium.logResults(_reportOptions.uiState("Recipe_card_scan")).run(axedriver);
+  }
+
+  @AfterClass
+  public static void reporting() throws IOException {
+    Runtime rt = Runtime.getRuntime();
+    String reporter = new File("src/test/resources/reporter-cli-macos").getAbsolutePath();
+    String Logger = new File("axe-json-reports/").getAbsolutePath();
+    String Destination = new File("a11y-results/").getAbsolutePath();
+    String command_xml =
+        reporter + " " + Logger + " --destination " + Destination + " --format xml";
+    String command_html =
+        reporter + " " + Logger + " --destination " + Destination + " --format html";
+    String command_csv =
+        reporter + " " + Logger + " --destination " + Destination + " --format csv";
+    try {
+      rt.exec(command_html);
+      rt.exec(command_csv);
+      rt.exec(command_xml);
+    } catch (Exception e) {
+      e.printStackTrace();
     }
-
-    @Test
-    public void test_a11y_workshop_homepage() {
-        driver.get("https://broken-workshop.dequelabs.com/");
-        Results results1 = axeSelenium.logResults(_reportOptions.uiState("Homepage_scan")).run(axedriver);
-    }
-
-    @Test
-    public void test_recipe_card_a11y_workshop_homepage(){
-        driver.get("https://broken-workshop.dequelabs.com/");
-        driver.findElement(By.cssSelector("#main-content > div.Recipes > div:nth-child(1) > div.Recipes__card-foot > button")).click();
-        Results results2 = axeSelenium.logResults(_reportOptions.uiState("Recipe_card_scan")).run(axedriver);
-
-    }
-
-    @AfterClass
-    public static void reporting()throws IOException{
-        Runtime rt = Runtime.getRuntime();
-        String command_xml = reporter +" "+ Logger+" --destination "+Destination+" --format xml";
-        String command_html = reporter +" "+ Logger+" --destination "+Destination+" --format html";
-        String command_csv = reporter +" "+ Logger+" --destination "+Destination+" --format csv";
-        try {
-            rt.exec(command_html);
-            rt.exec(command_csv);
-            rt.exec(command_xml);
-        }
-        catch(Exception e){e.printStackTrace();}
-        driver.quit();
-    }
+    driver.quit();
+  }
 }
