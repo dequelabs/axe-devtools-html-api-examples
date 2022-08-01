fe98ffb352d3a25bb65b85cc8070b8ac74af22f2
ankit-dq <ankit.kathal@deque.com>
2022-08-01 19:00:25 +0530

    Adding files to capybara-rspec example project

---
+++ Ruby/capybara-rspec/spec/example_spec.rb
@@ -0,0 +1,36 @@
+require 'axe-capybara'
+require 'axe-rspec'
+
+
+driver = AxeCapybara.configure(:chrome) do |c|
+  # see below for a full list of configurations
+#   c.jslib_path = "next-version/axe.js"
+end
+
+
+describe 'Analyse webpage for a11y issues'  do 
+  it 'Analyse broken-workshop homepage for a11y issues' do
+    @driver.page.visit "https://broken-workshop.dequelabs.com/"
+    expect(@driver.page).to be_audited_for_accessibility.logging_results({ui_state: 'homepage-analyse'})
+  end
+
+  it 'Analyse broken-workshop recipe card for a11y issues' do
+    @driver.page.visit "https://broken-workshop.dequelabs.com/"
+    @driver.page.find('#main-content > div.Recipes > div:nth-child(1) > div.Recipes__card-foot > button').click
+    expect(@driver.page).to be_audited_for_accessibility.logging_results({ui_state: 'recipe-card-analyse'})
+  end
+
+
+  after(:each) do
+    @driver.page.quit
+  end
+
+  after(:all) do
+    logs = (Dir.pwd)<<"/axe-reports"
+    reporter = (Dir.pwd)<<'/resources/reporter-cli-macos'
+    destination = (Dir.pwd)<<'/a11y-results'
+    command_html = reporter << " " << logs << " --destination " << destination << " --format html"
+    system command_html
+  end
+
+end 
