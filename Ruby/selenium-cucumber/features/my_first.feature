ce077616474ff36a4491c08abb7d5c6a2a6e1358
ankit-dq <ankit.kathal@deque.com>
2022-08-09 13:11:52 +0530

    Adding files to Selenium-Cucumber example project

---
+++ Ruby/selenium-cucumber/features/my_first.feature
@@ -0,0 +1,4 @@
+Feature: Login feature
+
+  Scenario: Check homepage for a11y issues
+    When I navigate to "https://broken-workshop.dequelabs.com/"
---
+++ Ruby/selenium-cucumber/features/support/env2.rb
@@ -0,0 +1,20 @@
+# # load selenium driver
+# require "selenium-webdriver"
+# # require driver of choice
+# require "axe-selenium"
+# # require 'webdrivers'
+
+# # Requiring 'axe-cucumber-steps' makes all of the axe-cucumber step
+# # definitions available to be used directly in your cucumber features.
+# require "axe-cucumber-steps"
+
+# Before do
+#   # configure AxeWatir
+#   @driver = AxeSelenium.configure(:chrome) do |c|
+#   end
+# end
+
+# # close browser when done
+# # After do
+# #   @driver.page.close
+# # end
