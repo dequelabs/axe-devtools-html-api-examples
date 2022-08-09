9358deb8a943221acb8963cb4c09ac9c1f004469
ankit-dq <ankit.kathal@deque.com>
2022-08-09 16:10:23 +0530

    Updating the selenium-cucumber package

--- Ruby/selenium-cucumber/features/step_definitions/custom_steps.rb
+++ Ruby/selenium-cucumber/features/step_definitions/custom_steps.rb
@@ -1,5 +1,8 @@
-require 'selenium-cucumber'
+When(/^I navigate to "(.*?)"$/) do |url|
+  @driver.page.navigate.to url
+end
 
-# Do Not Remove This File
-# Add your custom steps here
-# $driver is instance of webdriver use this instance to write your custom code
+
+Then('click on the recipar card button') do
+    @driver.page.find_element(css: '#main-content > div.Recipes > div:nth-child(1) > div.Recipes__card-foot > button').click
+  end
