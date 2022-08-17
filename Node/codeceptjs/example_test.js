71efe7743b7ae68fcb2af29ff6951f5a0e0849bd
ankit-dq <ankit.kathal@deque.com>
2022-08-17 19:08:09 +0530

    Updating the code to add 2 tests for homepage and recipecard

--- Node/codeceptjs/example_test.js
+++ Node/codeceptjs/example_test.js
@@ -1,9 +1,9 @@
-const playwright = require ('Playwright');
-const AxeDevtoolsPlaywright = require("@axe-devtools/playwright").default;
-const AxeDevtoolsBuilder = require('@axe-devtools/playwright').default;
+const AxeDevtoolsBuilder = require("@axe-devtools/playwright").default;
+const AxeDevtoolsReporter = require("@axe-devtools/reporter").default;
 Feature('Checking for a11y issues');
 
-Scenario('Checking homepage for a11y issues', ({ I }) => {
-    // I.amOnPage("https://broken-workshop.dequelabs.com/")
-    I.checkForA11yissues("https://broken-workshop.dequelabs.com/")
+Scenario('Checking homepage for a11y issues',async ({ I })  => {
+    await I.checkHomepageForA11yissues("https://broken-workshop.dequelabs.com/")
+    await I.checkRecipecardForA11yissues("https://broken-workshop.dequelabs.com/")
+    await I.createReports()
 });
--- Node/codeceptjs/steps_file.js
+++ Node/codeceptjs/steps_file.js
@@ -2,20 +2,48 @@
 const AxeDevtoolsBuilder = require("@axe-devtools/playwright").default;
 const AxeDevtoolsReporter = require("@axe-devtools/reporter").default;
 const playwright = require('playwright');
+reporter = new AxeDevtoolsReporter(
+  "DevTools-Reporter",
+  "./a11y-results/homepageFlows"
+);
 
 module.exports = function() {
   return actor({
-    checkForA11yissues: async function(url) {
+    checkHomepageForA11yissues: async function(url) {
       browser = await playwright.chromium.launch({ headless: false });
       const context = await browser.newContext();
       page = await context.newPage();
       await page.goto(url)
       const results = await new AxeDevtoolsBuilder({ page }).analyze()
-      console.log(results);
-
-    }
+      reporter.logTestResult("homepage-no-flow", results);
+      await browser.close();
+      // console.log(results);
+      }
     // Define custom steps here, use 'this' to access default methods of I.
     // It is recommended to place a general 'login' function here.
+    ,
+    checkRecipecardForA11yissues: async function(url) {
+      browser = await playwright.chromium.launch({ headless: false });
+      const context = await browser.newContext();
+      page = await context.newPage();
+      await page.goto(url)
+      await page.locator('#main-content > div.Recipes > div:nth-child(1) > div.Recipes__card-foot > button').click();
+      const results = await new AxeDevtoolsBuilder({ page }).analyze()
+      reporter.logTestResult("homepage-altered-state", results);
+      // console.log(results);
+      await browser.close();
+    }
+      
+      ,
 
+      createReports: async function() {
+      await reporter.buildHTML("./a11y-results/html/");
+      await reporter.buildJUnitXML("./a11y-results/xml/");
+      await reporter.buildCSV("./a11y-results/csv/");
+      // await browser.close();
+      
+    
+
+    }
   });
 }
