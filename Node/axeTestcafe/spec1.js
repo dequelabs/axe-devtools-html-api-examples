963e76cee9eee9b35a794fab9ffc903ab04dcc18
ankit-dq <102721614+ankit-dq@users.noreply.github.com>
2023-04-21 17:23:51 +0530

    Updating Node/axeTestcafe/spec1.js to incorporate the requested changes
    Co-authored-by: Michael <me@michaelsiek.com>

--- Node/axeTestcafe/spec1.js
+++ Node/axeTestcafe/spec1.js
@@ -1,27 +1,24 @@
-import axeTestCafe from './axeTestCafe';
-import AxeDevtoolsReporter from '@axe-devtools/reporter';
+import axeTestCafe from './axeTestCafe'
+import AxeDevtoolsReporter from '@axe-devtools/reporter'
+
 const reporter = new AxeDevtoolsReporter(
-    'DevTools-Reporter',
-    './a11y-results/json'
-  );
+  'DevTools-Reporter',
+  './a11y-results/json'
+)
 
-var axe;
+let axe
 
-fixture `A set of examples that illustrate how to use TestCafe API`
-    .page `https://broken-workshop.dequelabs.com/`
-    .before(async t =>{
-        axe = new axeTestCafe();
-    })
-    .after(async t =>{
-        await reporter.buildHTML('./a11y-results/html/')
-    })
-
-   
+fixture`A set of examples that illustrate how to use TestCafe API`
+  .page`https://broken-workshop.dequelabs.com/`
+  .before(async () => {
+    axe = new axeTestCafe()
+  })
+  .after(async () => {
+    await reporter.buildHTML('./a11y-results/html/')
+  })
 
 test('Accessibility quick scan', async t => {
-    const results = await axe.analyze(t);
-    // 
-    console.log(results);
-    reporter.logTestResult('TestcafeScan2_1', results);
-    // await t.expect(results.violations.length).eql(0);    
-});
+  const results = await axe.analyze(t)
+  reporter.logTestResult('TestcafeScan2_1', results)
+  await t.expect(results.violations.length).eql(0)
+})
